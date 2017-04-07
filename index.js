ab1c60770df0f112e416ff5df877fbb15ae65124
Timothy Cox <tcox@tdkc.com>
2017-04-07 17:03:05 -0400

    add node js and http-server

---
+++ index.js
@@ -0,0 +1,20 @@
+//require('kit.js')
+
+const http = require('http');
+const port = 3000;
+
+const requestHandler = (request, response) => {
+    console.log(request.url);
+    response.end('Hello Node.js Server');
+}
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err);
+    }
+
+    console.log('server is listeningon port');
+})
+
